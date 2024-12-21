ca8f53c44881ec2e203bc6cef86f072b1fd3a80b
smit-shah-GG <smitshahofficial@gmail.com>
2024-12-21 21:24:13 +0530

    run changes, because they were lost in git last time.

--- 2_serverer.py
+++ 2_serverer.py
@@ -846,4 +846,6 @@ def server_error(e):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(
+        host="0.0.0.0", port=int(os.getenv("PORT", 5000)), debug=app.config["DEBUG"]
+    )
